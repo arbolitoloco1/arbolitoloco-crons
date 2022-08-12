c1d0678768f85b726b02add11ce07d2428bc82c1
ArbolitoLoco <santikzk1406@gmail.com>
2022-08-12 19:31:19 -0300

    Adding PostGameDataUpload

---
+++ postgame_upload/postgame_data_upload.py
@@ -0,0 +1,65 @@
+from mwrogue.esports_client import EsportsClient
+from mwrogue.auth_credentials import AuthCredentials
+from bayesapiwrapper.bayesapiwrapper import BayesApiWrapper
+import json
+
+
+class PostGameDataUpload(object):
+    METADATA_TEMPLATE = """{{{{PostgameJsonMetadata
+|RiotPlatformId={}
+|RiotGameId={}
+|RiotHash={}
+|GameId={}
+|MatchId={}
+|N_GameInMatch={}
+|OverviewPage={}
+|DataPage={}
+}}}}"""
+
+    def __init__(self, site: EsportsClient):
+        self.site = site
+        self.new_games = None
+        self.bayes_api_wrapper = BayesApiWrapper()
+
+    def run(self):
+        self.query_new_games()
+        self.upload_games_data()
+
+    def query_new_games(self):
+        self.new_games = self.site.cargo_client.query(
+            tables="MatchScheduleGame=MSG, PostgameJsonMetadata=PJM",
+            where="MSG.RiotPlatformGameId IS NOT NULL AND MSG.HasRpgidInput = '1' AND (MSG.GameId != PJM.GameId "
+                  "OR MSG.MatchId != PJM.MatchId OR MSG.N_GameInMatch != PJM.N_GameInMatch OR "
+                  "MSG.OverviewPage != PJM.OverviewPage OR PJM.RiotPlatformGameId IS NULL)",
+            join_on="MSG.VersionedRpgid=PJM.VersionedRpgid",
+            fields="MSG.RiotPlatformGameId, MSG._pageName=Page, MSG.GameId, MSG.MatchId, MSG.RiotPlatformId, "
+                   "MSG.RiotHash, MSG.N_GameInMatch, MSG.OverviewPage, MSG.RiotGameId"
+        )
+
+    def upload_games_data(self):
+        for game in self.new_games:
+            riot_platform_game_id = game["RiotPlatformGameId"].replace("_", " ")
+            if not self.site.client.pages[f"V5 data:{riot_platform_game_id}"].exists:
+                try:
+                    data, timeline = self.bayes_api_wrapper.get_game(game["RiotPlatformGameId"])
+                except Exception as e:
+                    print(f"Skipping {game['RiotPlatformGameId']}")
+                    print(e)
+                    continue
+                self.site.save_title(title=f"V5 data:{riot_platform_game_id}",
+                                     text=json.dumps(data))
+                self.site.save_title(title=f"V5 data:{riot_platform_game_id}/Timeline",
+                                     text=json.dumps(timeline))
+            metadata_text = self.METADATA_TEMPLATE.format(game["RiotPlatformGameId"].split("_")[0],
+                                                          game['RiotPlatformGameId'].split("_")[1],
+                                                          "", game["GameId"], game["MatchId"],
+                                                          game["N GameInMatch"], game["OverviewPage"], game["Page"])
+            self.site.save_title(title=f"V5 metadata:{riot_platform_game_id}",
+                                 text=metadata_text)
+            print(f"Saved {game['RiotPlatformGameId']}")
+
+
+if __name__ == "__main__":
+    credentials = AuthCredentials(user_file="bot")
+    site = EsportsClient("lol", credentials=credentials)
+    PostGameDataUpload(site).run()
