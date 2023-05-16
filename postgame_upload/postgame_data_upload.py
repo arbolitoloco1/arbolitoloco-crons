eb2f67d9999703acac14b495944fba437e864d1e
Santiago Kozak <santikzk1406@gmail.com>
2023-05-16 04:23:30 -0300

    Adding new Live Regions

--- postgame_upload/postgame_data_upload.py
+++ postgame_upload/postgame_data_upload.py
@@ -18,7 +18,7 @@ class PostGameDataUpload(object):
 |OverviewPage={}
 |DataPage={}
 }}}}"""
-    RIOT_LIVE_PLATFORMS = ["BR1", "EUN1", "EUW1", "JP1", "KR", "LA1", "LA2", "NA1", "OC1", "TR1", "RU"]
+    RIOT_LIVE_PLATFORMS = ["BR1", "EUN1", "EUW1", "JP1", "KR", "LA1", "LA2", "NA1", "OC1", "TR1", "RU", "PH2", "SG2", "TH2", "TW2", "VN2"]
     IGNORED_PLATFORMS = ["VN"]
 
     def __init__(self, site: EsportsClient):
